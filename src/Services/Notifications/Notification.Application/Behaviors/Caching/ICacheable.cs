f676a942013427fed7ba9c7af0926cecee130767
Данил Михайлов <mylongcode@gmail.com>
2026-02-03 21:53:09 +0300

    добавил caching pipeline behavior

---
+++ src/Services/Notifications/Notification.Application/Behaviors/Caching/ICacheable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications.Application.Behaviors.Caching
+{
+    public interface ICacheable
+    {
+        bool BypassCache { get; }
+        string CacheKey { get; }
+        int SlidingExpirationInMinutes { get; }
+        int AbsoluteExpirationInMinutes { get; }
+    }
+}
--- src/Services/Notifications/Notification.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Notifications/Notification.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 using Notifications.Application.Behaviors;
+using Notifications.Application.Behaviors.Caching;
 namespace Notifications.Application.Extensions
 {
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddNotificationsApplication(this IServiceCollection services)
         {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceCollectionExtensions).Assembly));
             return services;
