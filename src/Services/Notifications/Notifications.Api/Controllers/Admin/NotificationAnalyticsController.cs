f1283287775a211a4559c5ea6057df44aceb7b74
Данил Михайлов <mylongcode@gmail.com>
2026-02-06 22:15:01 +0300

    добавил контроллер для аналитики

---
+++ src/Services/Notifications/Notifications.Api/Controllers/Admin/NotificationAnalyticsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notifications.Api.Controllers.Admin
+{
+    [ApiController]
+    [Route("/api/admin/analytics")]
+    public sealed class NotificationAnalyticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public NotificationAnalyticsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+
+    }
+}
